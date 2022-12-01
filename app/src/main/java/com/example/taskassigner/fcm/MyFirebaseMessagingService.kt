e75171ad6e7277eb572a97f433c508e5b4261c49
Orhan Uckulac <orhanuckulac0@gmail.com>
2022-12-01 11:32:34 +0300

    46th commit - setup Firebase Cloud Messaging

---
+++ app/src/main/java/com/example/taskassigner/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,90 @@
+package com.example.taskassigner.fcm
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.taskassigner.R
+import com.example.taskassigner.activities.MainActivity
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    // 2 types of messages, data messages and notification messages
+    // data messages are handled here, onMessageReceived
+    // traditionally used with GCM, Google Cloud Messaging
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        Log.d(TAG,"FROM ${message.from}")
+
+        message.data.isNotEmpty().let {
+            Log.d(TAG,"Message Data Payload: ${message.data}")
+        }
+
+        message.notification.let {
+            Log.d(TAG,"Message Notification Body: ${it?.body}")
+        }
+    }
+
+    /**
+     * Called if the FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        Log.e(TAG,"Refreshed Token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?){
+
+    }
+
+    private fun sendNotification() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // put this activity on top of the activity stacks
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        }
+
+        val channelId = this.resources.getString(R.string.default_notification_channel_id)
+        val defaultSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_stat_ic_notification)
+            .setContentTitle("Title")
+            .setContentText("Message")
+            .setAutoCancel(true)
+            .setSound(defaultSoundURI)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService( Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(channelId, "Channel TaskAssigner", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
